94c94416ded824c99da9c6cecac36b8793570de0
G00247892 <g00247892@gmit.ie>
2017-01-29 21:09:54 +0000

    Add files via upload

--- Scripts/Guard.cs
+++ Scripts/Guard.cs
@@ -21,8 +21,32 @@ public class Guard : MonoBehaviour {
 
 	// Update is called once per frame
 	void FixedUpdate () {
-
+        fpsTargetDistance = Vector3.Distance(fpsTarget.position, transform.position);
+        if (fpsTargetDistance < enemyLookDistance){
+           // myRender.material.color = Color.yellow;
+            LookAtPlayer();
+            print("Look at player please");
+        }
+        if(fpsTargetDistance < attackDistance)
+        {
+           // myRender.material.color = Color.red;
+            AttackPlease();
+            print("ATTACK");
+        }
+        else
+        {
+            //myRender.material.color = Color.blue;
+        }
 
 	}
 
+    void LookAtPlayer(){
+        Quaternion rotation = Quaternion.LookRotation(fpsTarget.position - transform.position);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime*damping);
+    }
+
+    void AttackPlease()
+    {
+       // theRigidBody.AddForce(transform.forward*enemyMovementSpeed);
+    }
 }
