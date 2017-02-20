7730ec886283dc0d930fc4acf285b9ad1bfb0b5c
G00247892 <g00247892@gmit.ie>
2017-02-20 18:29:59 +0000

    Add files via upload
    Basic guard, will follow, but won't stay upright when it reaches player. less wobbly if the guard is changed to a cube, using a capsule makes it buggy

---
+++ Scripts/BasicGuard.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicGuard : MonoBehaviour {
+
+    Transform target;
+    private int maxRange;
+    private int minRange;
+    private int maxDistance;
+    private int moveSpeed = 5;
+    private int rotationSpeed = 5;
+    private Transform myTransform;
+
+    void Awake()
+    {
+        myTransform = transform;
+    }
+    // Use this for initialization
+    void Start()
+    {
+        GameObject go = GameObject.FindGameObjectWithTag("Player");
+
+        target = go.transform;
+
+        maxDistance = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.DrawLine(target.position, myTransform.position, Color.yellow);
+
+        //Look at target
+        myTransform.rotation = Quaternion.Slerp(myTransform.rotation, Quaternion.LookRotation(target.position - myTransform.position), rotationSpeed * Time.deltaTime);
+
+        if (Vector3.Distance(target.position, myTransform.position) > maxDistance)
+        {
+            //Move towards target
+            myTransform.position += myTransform.forward * moveSpeed * Time.deltaTime;
+        }
+    }
+}
+
