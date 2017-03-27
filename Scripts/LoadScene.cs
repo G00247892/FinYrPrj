95e4aad42bff87c264c0cfabd50213172fd40ce2
G00247892 <g00247892@gmit.ie>
2017-03-27 23:19:13 +0100

    Add files via upload
    Trying to switch to another scene on a trigger event - currently not working properly

---
+++ Scripts/LoadScene.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+       // Application.LoadLevel("TestScene");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void OnTrigger(Collider other)
+    {
+        if(other.tag=="Player")
+        {
+            SceneManager.LoadScene("TestScene");
+        }
+    }
+}
