5aa0ddd77246516673779ab4f2e218858db3e027
G00247892 <g00247892@gmit.ie>
2017-04-23 17:18:06 +0100

    Add files via upload
    Added a script where pressing X damages health. When it hits 0 console will say player is dead.

---
+++ Scripts/CharacterHealth.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CharacterHealth : MonoBehaviour {
+    public float CurrentHealth { get; set;}
+    public float MaxHealth { get; set;}
+
+    public Slider healthbar;
+
+    // Use this for initialization
+    void Start () {
+        MaxHealth = 20f;
+        //Reset health to full on game load/reload
+        CurrentHealth = MaxHealth;
+
+        healthbar.value = CalcHealth();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.X))
+            DealDamage(6);
+	}
+
+    void DealDamage(float damageValue)
+    {
+        //Test function, damage player's health
+        CurrentHealth -= damageValue;
+        healthbar.value = CalcHealth();
+        //If player runs out of health kill player
+        if (CurrentHealth <= 0)
+            Die();
+    }
+
+    float CalcHealth()
+    {
+        return CurrentHealth / MaxHealth;
+    }
+
+    void Die() {
+        CurrentHealth = 0;
+        Debug.Log("You Died");
+    }
+
+}
