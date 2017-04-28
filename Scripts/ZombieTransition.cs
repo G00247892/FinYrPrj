685c0e7a2b263f67bef1e27abd027eabd592a5eb
G00247892 <g00247892@gmit.ie>
2017-04-28 15:56:21 +0100

    Add files via upload
    Zombie can attack and kill player - animations doesn't work yet.

---
+++ Scripts/ZombieTransition.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZombieTransition : MonoBehaviour {
+    public float speed = 10f;
+    public Vector3 maxVelocity = new Vector3(3, 5);
+    public GameObject Zombie;
+    public Animator zombieAnim;
+    public GameObject Player;
+    float damage = .1f;
+
+    void Awake()
+    {
+        zombieAnim = Zombie.GetComponent<Animator>();
+    }
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        //GetComponent<Animation>().Play();
+        print("Attack");
+        other.gameObject.GetComponent<CharacterHealth>().TakeDamage(damage);
+    }
+}
