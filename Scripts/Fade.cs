bf01750cb9ab199cc8179e69341d087597b07125
G00247892 <g00247892@gmit.ie>
2017-04-29 16:09:18 +0100

    Add files via upload
    Currently doesn't work as intended but it does kinda do what I want it to. It fades the scene but not the way I want it to

---
+++ Scripts/Fade.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Fade : MonoBehaviour {
+    public static Fade Instance{ set; get;}
+
+    public Image fadeImage;
+    private bool isInTransition;
+    private float transition;
+    private bool isShowing;
+    private float duration;
+
+    private void Awake()
+    {
+        Instance = this;
+        DontDestroyOnLoad(transform.gameObject);
+    }
+
+    public void Fader(bool showing, float duration)
+    {
+        isShowing = showing;
+        isInTransition = true;
+        this.duration = duration;
+        transition = (isShowing) ? 0 : 1;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.X))
+            Fader(true, 1.25f);
+
+        if (!isInTransition)
+            return;
+        transition += (isShowing) ? Time.deltaTime * (1 / duration) : -Time.deltaTime * (1 / duration);
+        fadeImage.color = Color.Lerp(new Color(1, 1, 1, 0), Color.white, transition);
+        if (transition > 1 || transition < 0)
+            isInTransition = false;
+    }
+}
